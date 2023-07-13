114a879826d9758cc8da6c646154ab4a353a4224
MastaNerd <78389274+MastaNerd@users.noreply.github.com>
2023-07-13 09:06:00 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -2,6 +2,8 @@ class Solution:
     def digitdecrypt(self, num):
             #type num: int
             #return type: int
+            if num == 0:
+                return 0
             identification = (num%10) + (num - num%10)/10
             return identification%9
             #TODO: Write code below to returnn an int with the solution to the prompt.
