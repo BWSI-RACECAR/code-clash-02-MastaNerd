f4f4cb962aae6d2d09c05b9c061df96d32a86ece
MastaNerd <78389274+MastaNerd@users.noreply.github.com>
2023-07-13 09:11:56 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -3,11 +3,11 @@ class Solution:
             #type num: int
             #return type: int
             identification = (num%10) + (num - num%10)/10
-             if identification == 9:
+            if identification == 9:
                  return 9
             return identification%9
             #TODO: Write code below to returnn an int with the solution to the prompt.
-       
+            
  
 def main():
     input1= input()
