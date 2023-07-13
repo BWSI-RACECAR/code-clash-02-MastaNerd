5d79d18e09ff0e50461e3da2c903a0b2c7ac4642
MastaNerd <78389274+MastaNerd@users.noreply.github.com>
2023-07-13 09:04:34 -0400

    Code_clash2

---
+++ digitdecrypt.py
@@ -0,0 +1,20 @@
+class Solution:    
+    def digitdecrypt(self, num):
+            #type num: int
+            #return type: int
+            identification = (num%10) + (num - num%10)/10
+            return identification%9
+            #TODO: Write code below to returnn an int with the solution to the prompt.
+            pass
+ 
+def main():
+    input1= input()
+    input2 = int(input1)
+
+    
+    tc1 = Solution()
+    ans = tc1.digitdecrypt(input2)
+    print(ans)
+    
+if __name__ == "__main__":
+    main()
